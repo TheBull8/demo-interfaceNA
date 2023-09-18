e5723f61e3ea3cee0e3a9409f32ab3f6aaf79cea
unknown <Andrus0106@outlook.com>
2023-09-18 09:23:02 +0300

    fix SIde bar ROute

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,42 +1,49 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom'
 const Sidebar = () => {
 
     return (
         <div className="sidebar">
             <ul className="sidebar-menu d-flex justify-center align-items-center">
-                <li><a href="/">
-                    <div className="text-center">
-                        <img className="mx-auto" src="/images/chat.svg" width="40" height="40" />
-                    </div>
-                    <div className="text-primary text-sm text-center mx-auto">
-                        <h6>
-                            Assistant
-                        </h6>
-                    </div>
-                </a></li>
-                <li><a href="/">
-                    <div className="text-center">
-                        <img className="mx-auto" src="/images/cart.svg" width="40" height="40" />
-                    </div>
-                    <div className="text-sm text-black text-center mx-auto">
-                        <h6>
-                            Shop
-                        </h6>
-                    </div>
-                </a></li>
+                <li>
+                    <Link className="  text-white mr-3" to="/">
+                        <div className="text-center">
+                            <img className="mx-auto" src="/images/chat.svg" width="40" height="40" />
+                        </div>
+                        <div className="text-primary text-sm text-center mx-auto">
+                            <h6>
+                                Assistant
+                            </h6>
+                        </div>
+                    </Link>
+                </li>
+                <li>
+                    <Link className="  text-white mr-3" to="/">
+                        <div className="text-center">
+                            <img className="mx-auto" src="/images/cart.svg" width="40" height="40" />
+                        </div>
+                        <div className="text-sm text-black text-center mx-auto">
+                            <h6>
+                                Shop
+                            </h6>
+                        </div>
+                    </Link>
+
+                </li>
             </ul>
             <ul className="sidebar-menu d-flex justify-center align-items-center">
-                <li><a href="/">
-                    <div className="text-center">
-                        <img className="mx-auto" src="/images/faq.svg" width="40" height="40" />
-                    </div>
-                    <div className="text-sm text-black text-center mx-auto">
-                        <h6>
-                            Faq
-                        </h6>
-                    </div>
-                </a></li>
+                <li>
+                    <Link className="  text-white mr-3" to="/">
+                        <div className="text-center">
+                            <img className="mx-auto" src="/images/faq.svg" width="40" height="40" />
+                        </div>
+                        <div className="text-sm text-black text-center mx-auto">
+                            <h6>
+                                Faq
+                            </h6>
+                        </div>
+                    </Link>
+                </li>
             </ul>
 
 
