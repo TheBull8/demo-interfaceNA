6c7913d258bbc15aaecbe7238daae5678e44b7f3
Edward Wong <48039229+TheBull8@users.noreply.github.com>
2023-11-17 17:34:40 +0700

    Update navbar.tsx
    Removed Auth0

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import { Link } from "react-router-dom";
-import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Navbar = () => {
   // login user
